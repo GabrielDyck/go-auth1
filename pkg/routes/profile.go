1047198ae71d9c948a9569464a805f153728bb15
Gabriel Fernando Dyck <gabriel.dyck@despegar.com>
2020-10-10 11:44:58 -0300

    profile edit

--- pkg/routes/profile.go
+++ pkg/routes/profile.go
@@ -1,16 +1,24 @@
 package routes
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 const (
-	profile="/profile/{id}"
+	profile = "/profile/{id}"
 )
 
-func addProfileRoutes(router *mux.Router){
+type ProfileWriteReq struct {
+	Email    string `json:"email"`
+	Fullname string `json:"fullname"`
+	Address  string `json:"address"`
+	Phone    string `json:"phone"`
+}
+
+func addProfileRoutes(router *mux.Router) {
 	router.HandleFunc(profile, func(writer http.ResponseWriter, request *http.Request) {
 		id := mux.Vars(request)["id"]
 
@@ -18,5 +26,33 @@ func addProfileRoutes(router *mux.Router){
 
 	}).Methods("GET")
 
+	router.HandleFunc(profile, func(writer http.ResponseWriter, request *http.Request) {
+		id := mux.Vars(request)["id"]
 
+		var req ProfileWriteReq
+		err := parseRequest(writer, request,&req)
+		if err!=nil {
+			return
+		}
+		fmt.Println(id)
+		fmt.Println(req)
+
+		err=validateRequiredFields(req)
+		if err !=nil{
+			wrapBadRequest(writer,err)
+			return
+		}
+
+	}).Methods("POST")
+
+}
+
+
+
+func validateRequiredFields(req ProfileWriteReq) error{
+
+	if req.Email == "" {
+		return errors.New("email cannot be empty")
+	}
+	return nil
 }
